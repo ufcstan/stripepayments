79968e24dbc8b101c1f25aaebaa09b0e019b4af5
stanley blackman <ufcstan@gmail.com>
2015-08-09 15:34:11 -0500

    Adding emailing of purchase receipt

---
+++ app/mailers/purchase_mailer.rb
@@ -0,0 +1,24 @@
+class PurchaseMailer <ActionMailer::Base
+	layout 'purchase_mailer'
+	default from: "One Month Stripe Payments <ufcstan@gmail.com>"
+
+   def purchase_receipt purchase
+   	@purchase = purchase
+   	mail to: purchase.email, subject: "Thanks for your purchase!"
+   end
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+

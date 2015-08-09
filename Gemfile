5ca833a183004582ccbc13cfccf6c9b8eba96f5a
stanley blackman <ufcstan@gmail.com>
2015-08-09 01:01:44 -0500

    Implemented gemfiles for handling emailing in both dev and production

--- Gemfile
+++ Gemfile
@@ -21,6 +21,9 @@ gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
 # Use Figaro to allow secret keys to work local/online-managing credentials
 gem 'figaro'
 
+#Add letter opener for handling emails in development
+gem 'letter_opener', group: :development
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+
+
+#Must install the following once you submit credit card info to Heroku
+
+# ActionMailer::Base.smtp_settings = {
+#  :address        => 'smtp.sendgrid.net',
+#  :port           => '587',
+#  :authentication => :plain,
+#  :user_name      => ENV['SENDGRID_USERNAME'],
+#  :password       => ENV['SENDGRID_PASSWORD'],
+#  :domain         => 'heroku.com',
+#  :enable_startstls_auto => true
+# }
+

fe036df360bd78fc41aa59044fbd0ff35d7ee506
stanley blackman <ufcstan@gmail.com>
2015-08-09 14:50:20 -0500

    Added code for addon

--- config/environment.rb
+++ config/environment.rb
@@ -9,13 +9,13 @@ Rails.application.initialize!
 
 #Must install the following once you submit credit card info to Heroku
 
-# ActionMailer::Base.smtp_settings = {
-#  :address        => 'smtp.sendgrid.net',
-#  :port           => '587',
-#  :authentication => :plain,
-#  :user_name      => ENV['SENDGRID_USERNAME'],
-#  :password       => ENV['SENDGRID_PASSWORD'],
-#  :domain         => 'heroku.com',
-#  :enable_startstls_auto => true
-# }
+ ActionMailer::Base.smtp_settings = {
+	:address        => 'smtp.sendgrid.net',
+	:port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_startstls_auto => true
+
 
